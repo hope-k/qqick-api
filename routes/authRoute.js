fdb204a39bc762994684066e435c54d8f5be5f88
hope-k <hopekumordzie@gmail.com>
2022-07-18 10:26:23 +0800

    fixed bugs

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const protectedRoute = require('../middlewares/protectedRoute');
-
+const protectedRoute = require('../middlewares/protectedRoute')
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
