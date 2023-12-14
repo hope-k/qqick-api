70fe3183dc4cba51c9feea97012bb8cccf9cc871
hope-k <hopekumordzie@gmail.com>
2023-12-14 01:48:08 +0000

    cron job addedv2

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running' }).status(200)
+})
 app.use('/api', chatsRoute);
 app.use('/api', authRoute);
 app.use('/api', messageRoute);
